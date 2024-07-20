a2853544bd2e2f90d25a3c12a2cac455e8342eda
SidSri090593 <127278326+SidSri090593@users.noreply.github.com>
2024-07-21 00:05:17 +1000

    Add Get- CompanyCountry to the List of Codes

---
+++ ConnectWiseManageAPI/Public/Company/Country/Get-CWMCompanyCountry.ps1
@@ -0,0 +1,20 @@
+function Get-CWMCompanyCountry {
+    [CmdletBinding()]
+    param(
+        [Alias('countryId')]
+        [int]$id,
+        [switch]$count,
+        [string]$condition,
+        [ValidatePattern('\S* (desc|asc)')]
+        [string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all
+    )
+
+    $Endpoint = '/company/countries'
+    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
