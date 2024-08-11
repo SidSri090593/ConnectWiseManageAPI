e85236b0c31ff4fc9e626578ce19724b608e60e9
SidSri090593 <127278326+SidSri090593@users.noreply.github.com>
2024-08-11 21:58:33 +1000

    Add Update-CWMCompanySite.ps1

---
+++ ConnectWiseManageAPI/Public/Company/CompanySites/Update-CWMCompanySite.ps1
@@ -0,0 +1,23 @@
+function Update-CWMCompanySite {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Alias('companyId')]
+        [int]$id,
+        [Parameter(Mandatory = $true)]
+        [Alias('SiteId')]
+        [int]$Siteid,
+        [Parameter(Mandatory = $true)]
+        [validateset('add', 'replace', 'remove')]
+        [string]$Operation,
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [Parameter(Mandatory = $true)]
+        $Value
+    )
+
+    $Endpoint = "/company/companies/$($id)/sites/$($Siteid)"
+    Invoke-CWMUpdateMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
+    
