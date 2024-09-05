bfcdbd9b76cf48c7c97dac18ad9e015d27cbcd43
SidSri090593 <127278326+SidSri090593@users.noreply.github.com>
2024-09-05 14:39:57 +1000

    add Get-CWMInvoice.ps1

---
+++ ConnectWiseManageAPI/Public/Finance/Invoice/Get-CWMInovoice.ps1
@@ -0,0 +1,20 @@
+function Get-CWMInvoice {
+    [CmdletBinding()]
+    param(
+        [Alias('invoiceId')]
+        [int]$id,
+        [switch]$count,
+        [string]$condition,
+        [ValidatePattern('\S* (desc|asc)')]
+        [string]$orderBy,
+        [string]$childConditions,
+        [string]$customFieldConditions,
+        [int]$page,
+        [int]$pageSize,
+        [string[]]$fields,
+        [switch]$all
+    )
+
+    $Endpoint = '/finance/invoices'
+    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
+}
